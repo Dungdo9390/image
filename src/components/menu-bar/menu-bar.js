a64303f4f785ee0eb99f9941dd9b7856ec97a17b
dodung9390 <dohongdung.ctd@gmail.com>
2019-09-23 21:03:06 +0700

    locale

--- src/components/menu-bar/menu-bar.js
+++ src/components/menu-bar/menu-bar.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './menu-bar.scss';
 import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
+import {locale} from '../../config/locale'
 
 class MenuBar extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      lang: 'vi'
+    }
+    locale.setLanguage(this.state.lang)
+   
+
+  }
+  switchLanguage = () => {
+    this.setState({
+    lang: this.state.lang == 'vi' ? 'en' : 'vi'
+    })
+    locale.setLanguage(this.state.lang)
+    this.setState({})
+  }
     render() {
         return(
             <React.Fragment>
@@ -11,22 +28,27 @@ class MenuBar extends React.Component {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/trang-chu">Trang chủ</Nav.Link>
-      <Nav.Link href="/gioi-thieu">Giới thiệu</Nav.Link>
-      <NavDropdown title="Sản phẩm" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Thuốc kê đơn</NavDropdown.Item>
+      <Nav.Link href="/trang-chu">{locale.home}</Nav.Link>
+      <Nav.Link href="/gioi-thieu">{locale.profile}</Nav.Link>
+      <NavDropdown title={locale.product} id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">{locale.prescription_medication}</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Thuốc không kê đơn</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Thực phẩm chức năng</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Dụng cụ y tế</NavDropdown.Item>
       </NavDropdown>
-      <Nav.Link href="/tuyen-dung">Tuyển dụng</Nav.Link>
-      <Nav.Link href="/lien-he">Liên hệ</Nav.Link>
+      <Nav.Link href="/tuyen-dung">{locale.hiring}</Nav.Link>
+      <Nav.Link href="/lien-he">{locale.contact}</Nav.Link>
     </Nav>
     <Form inline>
-      <FormControl type="text" placeholder="Tìm Kiếm" className="mr-sm-2" />
-      <Button variant="outline-success">Tìm Kiếm</Button>
+      
+      <FormControl type="text" placeholder={locale.search} className="mr-sm-2" />
+      <Button variant="outline-success">{locale.search}</Button>
     </Form>
+    <Nav>
+    <Button onClick={() => this.switchLanguage()}>{locale.lang}</Button>
+      <Nav.Link href="#">{locale.logout}</Nav.Link>
+    </Nav>
   </Navbar.Collapse>
 </Navbar>
             </React.Fragment>
---
+++ src/config/locale.js
@@ -0,0 +1,34 @@
+import LocalizedStrings from 'react-localization'
+
+export const locale = new LocalizedStrings(
+  {
+      vi: {
+          lang: 'Tiếng Việt',
+          home: 'Trang Chủ',
+          profile: 'Giới Thiệu',
+          product: 'Sản Phẩm',
+          hiring: 'Tuyển Dụng',
+          contact: 'Liên Hệ',
+          login: 'Đăng Nhập',
+          logout: 'Đăng Xuất',
+          phone_number: 'Số Điện Thoại',
+          password: 'Mật Khẩu',
+          search: 'Tìm Kiếm',
+          prescription_medication: 'Thuốc kê đơn'
+      },
+      en: {
+        lang: 'English',
+        home: 'Home',
+        profile: 'Profile',
+        product: 'Product',
+        hiring: 'Hiring',
+        contact: 'Contact',
+        login: 'Login',
+        logout: 'Logout',
+        phone_number: 'Phone Number',
+        password: 'Password',
+        search: 'Search',
+        prescription_medication: 'Prescription Medication'
+    }
+  }  
+)
